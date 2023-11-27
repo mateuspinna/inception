a2fe0c58a8fb76c206ab75fdd9252da0b926631d
mateuspinna <mateuslousa@gmail.com>
2023-11-26 23:12:13 -0300

    Add: nginx e worpress containers

---
+++ src/requirements/nginx/tools/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DOMAIN="mpinna-l.42.fr"
+ALREADY_EXISTS='false'
+
+if [ -f "/etc/nginx/key/certify.crt" ] && [ -f "/etc/nginx/key/private.key" ]; then
+    ALREADY_EXISTS='true'
+fi
+
+if [ "$ALREADY_EXISTS" = 'false' ]; then
+    mkdir /etc/nginx/key
+    openssl genrsa -out /etc/nginx/key/private.key 2048
+    openssl req -new -x509 -key /etc/nginx/key/private.key -out /etc/nginx/key/certify.crt -days 365 -subj "/CN=$DOMAIN_NAME"
+else
+    echo "certificate already exists"
+fi
---
+++ src/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -f "/var/www/html/wp/wp-config.php" ]; then
+echo "switch to /wp"
+cd /var/www/html/wp
+
+
+
+echo "Installing and starting WordPress"
+        wget -q -O - https://wordpress.org/latest.tar.gz | tar -xz -C /var/www/html/wp --strip-components=1
+        chmod -R +rwx /var/www/html/wp
+
+        wp --path=/var/www/html/wp --allow-root config create --dbname="$SQL_DATABASE" --dbuser="$SQL_USER" --dbpass="$SQL_PASSWORD" --dbhost="mariadb":"3306" --dbprefix='wp_'
+        wp --path=/var/www/html/wp --allow-root core install --url="$DOMAIN_NAME" --title="$WORDPRESS_SITE_NAME" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
+        wp --path=/var/www/html/wp --allow-root user create "$WP_USER_EDITOR" "$WP_EMAIL_EDITOR" --role='editor' --user_pass="$WP_PASSWORD_EDITOr"
+        wp --path=/var/www/html/wp --allow-root user create "$WP_USER" "$WP_EMAIL" --role='subscriber' --user_pass="$WP_PASSWORD"
+fi
+
+chmod -R 777 /var/www/html/wp
+mkdir -p /run/php/
+chown www-data:www-data /run/php/
+
+exec php-fpm7.4 -F
